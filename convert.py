69581a140ea6b674d532eddc948d50397fd0df6e
Oláh Alex <Olah.Alex@stud.u-szeged.hu>
2022-11-17 19:33:39 +0100

    Add different case strategies

--- convert.py
+++ convert.py
@@ -1,13 +1,16 @@
 import subprocess
 import sys
 import os
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, LancasterStemmer
 from nltk.tokenize import word_tokenize
 import nltk
 import pandas as pd
+import uuid
+import numpy as np
 
 PATH_TO_PROJECT = "repos/project"
-RESULT_PATH = "result.csv"
+RESULT_PATH = "result"
+CSV = "csv"
 LOG_NAME = "log.txt"
 SEPARATOR = "-"
 
@@ -30,18 +33,24 @@ def get_git_log():
         "--full-history", 
         "--reverse", 
         # "--shortstat", we will deal with this later
-        "--format=%h,%an,%ai,%f"], stdout=f)
+        "--format=%h;%an;%as;%f"], stdout=f)
 
-def create_csv_from_git_log():
+def create_csv_from_git_log(case_grouping_strategy):
     header_names=["shaid","author","date","message","activity", "case id"]
-    df = pd.read_csv(LOG_NAME, sep=",", header=None, names=header_names)
+    df = pd.read_csv(LOG_NAME, sep=";", header=None, names=header_names, parse_dates=["date"])
+    df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
 
-    ps = PorterStemmer()
+    # ps = PorterStemmer()
+    ls = LancasterStemmer() # lancasterstemmer seems to produce rougher stemmings, which is good in our case
     
     for i in df.index:
         message = None
         author = None
-        df["case id"] = "case 1"
+
+        if case_grouping_strategy == "date":
+            df["case id"][i] = "case_" + str(df["date"][i].value)
+        elif case_grouping_strategy == "user":
+            df["case id"][i] = "case_" + str(df["author"][i].replace(" ", "_"))
 
         try:
             message = str(df["message"][i]).lower()
@@ -55,13 +64,13 @@ def create_csv_from_git_log():
             df["activity"][i] = "issue"
         elif is_bot(author):
             df["activity"][i] = "bot"
-        elif not starts_with_verb(message):
-            df["activity"][i] = "nonconventional"
+        # elif not starts_with_verb(message):
+        #     df["activity"][i] = "nonconventional"
         else:
             first_word = message.split(SEPARATOR)[0]
-            df["activity"][i] = ps.stem(first_word)
+            df["activity"][i] = ls.stem(first_word)
     
-    df.to_csv(RESULT_PATH, index=False, header=True)
+    df.to_csv(f"result_{str(uuid.uuid4())}.csv", index=False, header=True)
         
 # could be extended
 def is_bot(author):
@@ -79,12 +88,12 @@ def is_issue(message: str):
     return len(splits) > 1 and splits[1].isnumeric()
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
+    if len(sys.argv) <= 2:
         print("USAGE...")
     
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
     clone_repository(sys.argv[1])
     get_git_log()
-    create_csv_from_git_log()
+    create_csv_from_git_log(sys.argv[2])
     
