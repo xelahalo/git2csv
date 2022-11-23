977e416897c86f422a0c0af582b6ab77e42b5435
Oláh Alex <Olah.Alex@stud.u-szeged.hu>
2022-11-23 23:48:24 +0100

    Use files as traces

--- convert.py
+++ convert.py
@@ -8,11 +8,16 @@ import pandas as pd
 import uuid
 import numpy as np
 import pm4py
+import re
+import string
 
 RESULT_PATH = "result"
 CSV = "csv"
 LOG_NAME = "log.txt"
-SEPARATOR = "-"
+SEPARATOR = " "
+COMMON_WORDS = "common_title_words.txt"
+
+common_words = []
 
 def clone_repository(uri):
     # TODO: better exception handling
@@ -32,26 +37,107 @@ def get_git_log(folder):
         "git", 
         "log", 
         "--branches", 
-        "--tags", 
+        "--tags",
         "--remotes", 
         "--full-history", 
         "--reverse", 
-        # "--shortstat", we will deal with this later
+        "--stat",
         "--format=%h;%an;%as;%f"], stdout=f)
 
-def create_csv_from_git_log():
-    header_names=["id","author","date","subject_line","activity", "case id"]
-    df = pd.read_csv(LOG_NAME, sep=";", header=None, names=header_names, parse_dates=["date"])
-    df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
+def get_git_log_with_stats(folder):
+    os.chdir(folder)
+    with open(LOG_NAME, "w") as f:
+        subprocess.call([
+            "git",
+            "log",
+            "-w",
+            "--all",
+            "--numstat",
+            "--find-renames",
+            "--reverse",
+            "--encoding=UTF-8",
+            "--pretty=format:%H;%an;%at;%s"
+        ], stdout=f)
 
+def process_log():
+    header_regex = "(.*);(.*);(\d+);(.*)"
+    stats_regex = "\d+\s+\d+\s+(\S+)\s*"
+    rename_regex = "{(\S+)\s*=>\s*(\S+)}(\S+)"
+
+    ls = LancasterStemmer()
+
+    prev_header = None
+    rows_to_append = {}
+    file_case_counter = {}
+
+    with open(LOG_NAME, "r", encoding="UTF-8") as f:
+        for row in f:
+            header = re.match(header_regex, row)
+            if header is not None:
+                shaid, author, time, sl = header.groups()
+                prev_header = {"id": shaid, "author": author.lower(), "time": time, "subject_line": sl.lower()}
+                continue
+
+            stats = re.match(stats_regex, row)
+            if stats is not None:
+                filename = stats.groups()[0]
+                rename_match = re.match(rename_regex, filename)
+
+                if rename_match is not None:
+                    source, target, remaining_path = rename_match.groups()
+                    source = source + remaining_path
+                    target = target + remaining_path
+
+                    no_of_cases_for_file = file_case_counter[source]
+                    file_case_counter[target] = no_of_cases_for_file
+                    del file_case_counter[source]
+
+                    for i in range(no_of_cases_for_file):
+                        key = f"{source}_{str(i)}"
+                        new_key = f"{target}_{str(i)}"
+                        row = rows_to_append[key]
+                        rows_to_append[new_key] = row
+                        del rows_to_append[key]
+
+                    filename = target
+                
+                sl = prev_header["subject_line"]
+                author = prev_header["author"]
+
+                activity = get_activity(sl, author, ls)
+               
+                key = filename
+                if filename in file_case_counter.keys():
+                    val = file_case_counter[filename]
+                    key += f"_{str(val)}"
+                    file_case_counter[filename] = val + 1
+                else:
+                    key += "_0"
+                    file_case_counter[filename] = 1
+
+                rows_to_append[key] = {"id": prev_header["id"], "author": author, "time": prev_header["time"], "subject_line": sl, "activity": activity}
+
+    for key in rows_to_append.keys():
+        case_id = key.rstrip(string.digits)
+        rows_to_append[key]["case_id"] = hash(case_id)
+
+    df = pd.DataFrame.from_records(list(rows_to_append.values()))
+
+    write_xes(df)
+
+def create_xes_from_git_log():
+    header_names=["id","author","time","subject_line","activity", "case id"]
+    df = pd.read_csv(LOG_NAME, sep=";", header=None, names=header_names, parse_dates=["time"])
+    df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d")
+    
     # ps = PorterStemmer()
     ls = LancasterStemmer() # lancasterstemmer seems to produce rougher stemmings, which is good in our case
-    
+
     for i in df.index:
         message = None
         author = None
 
-        df["case id"][i] = "case_" + str(df["date"][i].value)
+        df["case id"][i] = "case_" + str(df["time"][i].value)
         # df["case id"][i] = "case_" + str(df["author"][i].replace(" ", "_"))
 
         try:
@@ -62,45 +148,58 @@ def create_csv_from_git_log():
             df["activitiy"][i] = "nonconventional"
             continue
 
-        if is_issue(message):
-            df["activity"][i] = "issue"
-        elif is_bot(author):
-            df["activity"][i] = "bot"
-        elif not starts_with_verb(message):
-            df["activity"][i] = "nonconventional"
-        else:
-            first_word = message.split(SEPARATOR)[0]
-            df["activity"][i] = ls.stem(first_word)
+        df["activity"][i] = get_activity(message, author, ls)
 
+        write_xes(df)
+
+def write_xes(df):
     filename=str(uuid.uuid4())
+    # TODO make it be aware of the traces
     for idx, chunk in enumerate(np.array_split(df, 10)):
-        chunk = pm4py.format_dataframe(chunk, case_id="case id", activity_key="activity", timestamp_key="date")
+        chunk = pm4py.format_dataframe(chunk, case_id="case_id", activity_key="activity", timestamp_key="time")
         log = pm4py.convert_to_event_log(chunk)
         pm4py.write_xes(log, f"../../results/{filename}_part{idx}.xes")
 
-        
+def get_activity(message, author, stemmer):
+    if is_issue(message):
+        return "issue"
+    elif is_bot(author):
+        return "bot"
+    elif not starts_with_verb(message):
+        return "nonconventional"
+    else:
+        first_word = message.split(SEPARATOR)[0]
+        return stemmer.stem(first_word)
+
 # could be extended
 def is_bot(author):
     return "bot" in author
 
 def starts_with_verb(message):
     VERB_CODES = ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "VERB"]
-    sentence = message.replace("-", " ").capitalize() + "."
-    code_for_wirst_word = nltk.pos_tag(word_tokenize(sentence))[0][1]
-    nltk.pos_tag(word_tokenize(sentence))
-    return code_for_wirst_word in VERB_CODES
+    sentence = message.capitalize() + "."
+    first_word = nltk.pos_tag(word_tokenize(sentence))[0]
+
+    return first_word[1] in VERB_CODES or first_word[0] in common_words
 
 def is_issue(message: str):
-    splits = message.split(SEPARATOR)
-    return len(splits) > 1 and splits[1].isnumeric()
+    regex = "\[.+-\d+\].*"
+    return re.match(regex, message) is not None
+
+def load_common_words():
+    with open(COMMON_WORDS, "r") as f:
+        common_words = f.readlines()
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("USAGE...")
     
-    nltk.download('punkt')
-    nltk.download('averaged_perceptron_tagger')
+    # nltk.download('punkt')
+    # nltk.download('averaged_perceptron_tagger')
+    load_common_words()
     folder = clone_repository(sys.argv[1])
-    get_git_log(folder)
-    create_csv_from_git_log()
-    
+    get_git_log_with_stats(folder)
+    # create_csv_from_git_log()
+    process_log()
+
+
